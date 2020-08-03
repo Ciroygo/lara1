4430707f7c615606533c760901838935ae8038f5
ciroygo <ciroygo2gmail.com>
2020-08-03 23:53:09 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
+
+
+// 登陆页、登陆操作、退出操作
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
