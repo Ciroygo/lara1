966dd61e82ca8e3e2a1e17fb521577b26e6bdf03
ciroygo <ciroygo2gmail.com>
2020-08-03 23:18:08 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,6 +8,7 @@
 @include('layouts._header')
 <div class="container">
   <div class="offset-md-1 col-md-10">
+    @include('shared._errors')
     @yield('content')
     @include('layouts._footer')
   </div>
